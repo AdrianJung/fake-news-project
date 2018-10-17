5ee6a8f0796a03b4d9e30199d763a0afaa37b7d0
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-10-17 10:28:06 +0200

    created php files and added content to data.php

---
+++ data.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+$data = [
+ [
+   'title'=> 'Lorem',
+   'content' => 'Ipsum',
+   'publishedDate' => date('Y'),
+   'likeCounter' => 6,
+   'author' =>
+   ['id' => 1,
+   'name' => 'Adrian',
+  ],
+ ],
+];
