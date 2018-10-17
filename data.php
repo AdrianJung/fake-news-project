16781cb5730ac01bfd8c245bfa0a0516153b78bd
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-10-17 18:11:59 +0200

    created content folder, updated data.php
    added articles in the content folder and updated the authors in the data.php, also updated the articles in data.php

--- data.php
+++ data.php
@@ -1,25 +1,25 @@
 <?php
 
-declare(strict_types=1);
 
-$data = [
-	'authors' => [
-		['id' => 1, 'name' => 'adrian Gotlandsson'],
-		['id' => 2, 'name' => 'Batman Batmansson'],
-		['id' => 3, 'name' => 'Robin Robinsson'],
-		['id' => 4, 'name' => 'Blabla Blablasson'],
-		['id' => 5, 'name' => 'Benjamin Fransson']
-	],
+$authors = [
+		['id' => 1, 'name' => 'Harry Potter'],
+		['id' => 2, 'name' => 'Ron Weasley'],
+		['id' => 3, 'name' => 'Albus Dumbledore'],
+		['id' => 4, 'name' => 'Severus Snape'],
+		['id' => 5, 'name' => 'Hermione Granger'],
 ];
 
-$article = [
-	'news' => [
-		['title' => 'Lorem', 'content' => 'ipsum', 'publishedDate' => date('Y'), 'likeCounter' => 5, 'author' => 1],
-		['title' => 'Lorem', 'content' => 'ipsum', 'publishedDate' => date('Y'), 'likeCounter' => 5, 'author' => 2],
-		['title' => 'Lorem', 'content' => 'ipsum', 'publishedDate' => date('Y'), 'likeCounter' => 5, 'author' => 3],
-		['title' => 'Lorem', 'content' => 'ipsum', 'publishedDate' => date('Y'), 'likeCounter' => 5, 'author' => 4],
-		['title' => 'Lorem', 'content' => 'ipsum', 'publishedDate' => date('Y'), 'likeCounter' => 5, 'author' => 5],
-	]
+$articles = [
+		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article1.txt'), 'publishedDate' => 2018-08-01, 'likeCounter' => 1, 'author' => 1],
+		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article2.txt'), 'publishedDate' => 2018-08-02, 'likeCounter' => 2, 'author' => 1],
+		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article3.txt'), 'publishedDate' => 2018-08-03, 'likeCounter' => 3, 'author' => 2],
+		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article4.txt'), 'publishedDate' => 2018-08-04, 'likeCounter' => 4, 'author' => 2],
+		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article5.txt'), 'publishedDate' => 2018-08-05, 'likeCounter' => 5, 'author' => 3],
+		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article6.txt'), 'publishedDate' => 2018-08-06, 'likeCounter' => 6, 'author' => 3],
+		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article7.txt'), 'publishedDate' => 2018-08-07, 'likeCounter' => 7, 'author' => 4],
+		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article8.txt'), 'publishedDate' => 2018-08-08, 'likeCounter' => 8, 'author' => 4],
+		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article9.txt'), 'publishedDate' => 2018-08-09, 'likeCounter' => 9, 'author' => 5],
+		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article10.txt'), 'publishedDate' => 2018-08-10, 'likeCounter' => 10, 'author' => 5],
 ];
 
 /*
