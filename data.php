1ccf085af62b9d852b86a87b397b1911e6d9a536
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-10-17 11:14:34 +0200

    added & updated files
    added bootswatch stylesheet, updated data.php,c reated editorconfig and started on index.php file

--- data.php
+++ data.php
@@ -1,15 +1,36 @@
 <?php
+
 declare(strict_types=1);
 
 $data = [
- [
-   'title'=> 'Lorem',
-   'content' => 'Ipsum',
-   'publishedDate' => date('Y'),
-   'likeCounter' => 6,
-   'author' =>
-   ['id' => 1,
-   'name' => 'Adrian',
-  ],
- ],
+	'authors' => [
+		['id' => 1, 'name' => 'adrian Gotlandsson'],
+		['id' => 2, 'name' => 'Batman Batmansson'],
+		['id' => 3, 'name' => 'Robin Robinsson'],
+		['id' => 4, 'name' => 'Blabla Blablasson'],
+		['id' => 5, 'name' => 'Benjamin Fransson']
+	],
+	'news' => [
+		['title' => 'Lorem', 'content' => 'ipsum', 'publishedDate' => date(), 'likeCounter' => 5, 'author' => 1],
+		['title' => 'Lorem', 'content' => 'ipsum', 'publishedDate' => date(), 'likeCounter' => 5, 'author' => 2],
+		['title' => 'Lorem', 'content' => 'ipsum', 'publishedDate' => date(), 'likeCounter' => 5, 'author' => 3],
+		['title' => 'Lorem', 'content' => 'ipsum', 'publishedDate' => date(), 'likeCounter' => 5, 'author' => 4],
+		['title' => 'Lorem', 'content' => 'ipsum', 'publishedDate' => date(), 'likeCounter' => 5, 'author' => 5],
+	]
 ];
+
+/**
+ * Ska lagras:
+ *
+ * # News articles
+ * id
+ * title
+ * content
+ * date
+ * likes
+ * author
+ *
+ * # Authors
+ * id
+ * name
+ */
---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__.'/functions.php';
+
+require __DIR__.'/data.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+	 <link rel="stylesheet" href="/css/bootswatch.css">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
