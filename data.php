797134d153cde4031a1b4ad218e774ea04e432ee
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-10-18 11:30:15 +0200

    That's just how I roll
    updated arrays, made loops for articles, changed external stylesheet

--- data.php
+++ data.php
@@ -1,30 +1,91 @@
 <?php
 
-
 $authors = [
-		['id' => 1, 'name' => 'Harry Potter'],
-		['id' => 2, 'name' => 'Ron Weasley'],
-		['id' => 3, 'name' => 'Albus Dumbledore'],
-		['id' => 4, 'name' => 'Severus Snape'],
-		['id' => 5, 'name' => 'Hermione Granger'],
+		['author_id' => 1,'name' => 'Harry Potter'],
+		['author_id' => 2, 'name' => 'Ron Weasley'],
+		['author_id' => 3, 'name' => 'Albus Dumbledore'],
+		['author_id' => 4, 'name' => 'Severus Snape'],
+		['author_id' => 5, 'name' => 'Hermione Granger'],
 ];
+
 
 $articles = [
-		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article1.txt'), 'publishedDate' => 2018-08-01, 'likeCounter' => 1, 'author' => 1],
-		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article2.txt'), 'publishedDate' => 2018-08-02, 'likeCounter' => 2, 'author' => 1],
-		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article3.txt'), 'publishedDate' => 2018-08-03, 'likeCounter' => 3, 'author' => 2],
-		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article4.txt'), 'publishedDate' => 2018-08-04, 'likeCounter' => 4, 'author' => 2],
-		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article5.txt'), 'publishedDate' => 2018-08-05, 'likeCounter' => 5, 'author' => 3],
-		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article6.txt'), 'publishedDate' => 2018-08-06, 'likeCounter' => 6, 'author' => 3],
-		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article7.txt'), 'publishedDate' => 2018-08-07, 'likeCounter' => 7, 'author' => 4],
-		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article8.txt'), 'publishedDate' => 2018-08-08, 'likeCounter' => 8, 'author' => 4],
-		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article9.txt'), 'publishedDate' => 2018-08-09, 'likeCounter' => 9, 'author' => 5],
-		['title' => 'Lorem', 'content' => file_get_contents(__DIR__.'/content/article10.txt'), 'publishedDate' => 2018-08-10, 'likeCounter' => 10, 'author' => 5],
+		['id' => 1,
+		 'title' => 'Blue',
+		 'content' => file_get_contents(__DIR__.'/content/article1.txt'),
+		 'date' => '2018-08-01',
+		 'likeCounter' => 1],
+
+		['id' => 1,
+		 'title' => 'Blue',
+		 'content' => file_get_contents(__DIR__.'/content/article2.txt'),
+		 'Date' => '2018-08-02',
+		 'likeCounter' => 2],
+
+		['id' => 2,
+		 'title' => 'Green',
+		 'content' => file_get_contents(__DIR__.'/content/article3.txt'),
+		 'date' => '2018-08-03',
+		 'likeCounter' => 3],
+
+		['id' => 2,
+		 'title' => 'Green',
+		 'content' => file_get_contents(__DIR__.'/content/article4.txt'),
+		 'date' => '2018-08-04',
+		 'likeCounter' => 4],
+
+		['id' => 3,
+		 'title' => 'Red',
+		 'content' => file_get_contents(__DIR__.'/content/article5.txt'),
+		 'date' => '2018-08-05',
+		 'likeCounter' => 5],
+
+		['id' => 3,
+		 'title' => 'Red',
+     'content' => file_get_contents(__DIR__.'/content/article6.txt'),
+		 'date' => '2018-08-06',
+		 'likeCounter' => 6],
+
+		['id' => 4,
+		 'title' => 'Black',
+		 'content' => file_get_contents(__DIR__.'/content/article7.txt'),
+		 'date' => '2018-08-07',
+		 'likeCounter' => 7],
+
+		['id' => 4,
+		 'title' => 'Black',
+		 'content' => file_get_contents(__DIR__.'/content/article8.txt'),
+		 'date' => '2018-08-08',
+		 'likeCounter' => 8],
+
+		['id' => 5,
+		 'title' => 'Purple',
+		 'content' => file_get_contents(__DIR__.'/content/article9.txt'),
+		 'date' => '2018-08-09',
+		 'likeCounter' => 9],
+
+		['id' => 5,
+		 'title' => 'Purple',
+		 'content' => file_get_contents(__DIR__.'/content/article10.txt'),
+		 'date' => '2018-08-10',
+		 'likeCounter' => 10,],
 ];
 
-/*
-gör en separat array för news
- */
+
+
+
+// Test foreach loop -->
+// foreach ($authors as $author) {
+// 	foreach ($articles as $article) {
+// 		if ($author['id'] === 1 && $article['id'] === 1) {
+// 			echo $author['name']. "<br>" . $article['title']. "<br>" . $article['content'];
+// 		}
+// 	}
+// }
+
+
+// gör en separat array för news
+ //
 /**
  * Ska lagras:
  *
--- index.php
+++ index.php
@@ -2,30 +2,41 @@
 
 declare(strict_types=1);
 
+require __DIR__.'/data.php';
+
 require __DIR__.'/functions.php';
 
-require __DIR__.'/data.php';
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-	 <link rel="stylesheet" href="css/bootswatch.min.css">
-	 <link rel="stylesheet" href="css/main.css">
-    <title></title>
-  </head>
-  <body>
-	  <div class="card1">
-  		<div class="card border-primary mb-3" style="max-width: 20rem;">
-  			<div class="card-header">Header</div>
-  			<div class="card-body">
-	 			<h4 class="card-title">Primary card title</h4>
-	 			<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-	 </div>
-  </div>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/bootswatch.min.css">
+	<link rel="stylesheet" href="css/main.css">
+	<title></title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<?php foreach ($articles as $article):?>
+				<?php for ($i=0; $i < count($authors); $i++):?>
+					<?php if ($authors[$i]['author_id'] === $article['id']):?>
+						<div class="col-4 p-4">
+							<div class="card text-black bg-primary-mb3" style="max-width: 20rem;">
+								<div class="card-header p-4"><?=$authors[$i]['name']?></div>
+							<?php endif; ?>
+						<?php endfor; ?>
+						<div class="card-body">
+							<h4 class="card-title"><?= $article['title']?></h4>
+							<p class="card-text"><?= $article['content']?></p>
+						</div>
+					</div>
+				</div>
+			<?php endforeach;?>
+		</div>
+	</div>
 </div>
-
-  </body>
+</body>
 </html>
