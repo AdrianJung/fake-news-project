ca889b0c9760225325af7c34dc3237bf260d7500
Adrian Jungnelius <a.jungnelius@gmail.com>
2018-10-19 13:31:20 +0200

    updated array, added button
    added a like button in the index.php file

--- data.php
+++ data.php
@@ -11,93 +11,62 @@ $authors = [
 
 $articles = [
 		['id' => 1,
-		 'title' => 'Blue',
+		 'title' => 'This title',
 		 'content' => file_get_contents(__DIR__.'/content/article1.txt'),
 		 'date' => '2018-08-01',
 		 'likeCounter' => 1],
 
-		['id' => 1,
-		 'title' => 'Blue',
+		['id' => 2,
+		 'title' => 'That title',
 		 'content' => file_get_contents(__DIR__.'/content/article2.txt'),
 		 'Date' => '2018-08-02',
 		 'likeCounter' => 2],
 
-		['id' => 2,
-		 'title' => 'Green',
+		['id' => 4,
+		 'title' => 'Another title',
 		 'content' => file_get_contents(__DIR__.'/content/article3.txt'),
 		 'date' => '2018-08-03',
 		 'likeCounter' => 3],
 
-		['id' => 2,
-		 'title' => 'Green',
+		['id' => 3,
+		 'title' => 'Why do you need a title?',
 		 'content' => file_get_contents(__DIR__.'/content/article4.txt'),
 		 'date' => '2018-08-04',
 		 'likeCounter' => 4],
 
-		['id' => 3,
-		 'title' => 'Red',
+		['id' => 5,
+		 'title' => 'TITLE',
 		 'content' => file_get_contents(__DIR__.'/content/article5.txt'),
 		 'date' => '2018-08-05',
 		 'likeCounter' => 5],
 
-		['id' => 3,
-		 'title' => 'Red',
+		['id' => 4,
+		 'title' => 'The most interesting article',
      'content' => file_get_contents(__DIR__.'/content/article6.txt'),
 		 'date' => '2018-08-06',
 		 'likeCounter' => 6],
 
-		['id' => 4,
-		 'title' => 'Black',
+		['id' => 1,
+		 'title' => 'Read this now',
 		 'content' => file_get_contents(__DIR__.'/content/article7.txt'),
 		 'date' => '2018-08-07',
 		 'likeCounter' => 7],
 
-		['id' => 4,
-		 'title' => 'Black',
+		['id' => 3,
+		 'title' => 'Super important article',
 		 'content' => file_get_contents(__DIR__.'/content/article8.txt'),
 		 'date' => '2018-08-08',
 		 'likeCounter' => 8],
 
 		['id' => 5,
-		 'title' => 'Purple',
+		 'title' => 'Probably the best article here',
 		 'content' => file_get_contents(__DIR__.'/content/article9.txt'),
 		 'date' => '2018-08-09',
 		 'likeCounter' => 9],
 
-		['id' => 5,
-		 'title' => 'Purple',
+		['id' => 2,
+		 'title' => 'Such article, much wow',
 		 'content' => file_get_contents(__DIR__.'/content/article10.txt'),
 		 'date' => '2018-08-10',
 		 'likeCounter' => 10,],
 ];
-
-
-
-
-// Test foreach loop -->
-// foreach ($authors as $author) {
-// 	foreach ($articles as $article) {
-// 		if ($author['id'] === 1 && $article['id'] === 1) {
-// 			echo $author['name']. "<br>" . $article['title']. "<br>" . $article['content'];
-// 		}
-// 	}
-// }
-
-
-// gör en separat array för news
- //
-/**
- * Ska lagras:
- *
- * # News articles
- * id
- * title
- * content
- * date
- * likes
- * author
- *
- * # Authors
- * id
- * name
- */
--- index.php
+++ index.php
@@ -13,8 +13,9 @@ require __DIR__.'/functions.php';
 <html lang="en" dir="ltr">
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="css/bootswatch.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 	<title></title>
 </head>
 <body>
@@ -27,14 +28,20 @@ require __DIR__.'/functions.php';
 			<?php foreach ($articles as $article):?>
 				<?php for ($i=0; $i < count($authors); $i++):?>
 					<?php if ($authors[$i]['author_id'] === $article['id']):?>
-						<div class="col-4 p-4">
-							<div class="card text-black bg-primary-mb3" style="max-width: 20rem;">
-								<div class="card-header p-4"><?=$authors[$i]['name']?></div>
+						<div class="col-12 p-4">
+							<div class="card text-white bg-dark">
+								<div class="card-header p-4"><h4><?=$authors[$i]['name']?></h4></div>
 							<?php endif; ?>
 						<?php endfor; ?>
 						<div class="card-body">
-							<h4 class="card-title"><?= $article['title']?></h4>
+							<h5 class="card-title"><?= $article['title']?></h5>
 							<p class="card-text"><?= $article['content']?></p>
+							<div class="d-flex justify-content-end">
+							<button type="button" class="btn btn-dark">
+								<span class="fas fa-thumbs-up"></span>
+								<span class"likes"><?=$article['likeCounter']?></span>
+								</button>
+							</div>
 						</div>
 					</div>
 				</div>
